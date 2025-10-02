23d93f2d2eeaacafb99d015ab184e5a7c748207c
Monica  Rojas <monicarojas1711@gmail.com>
2025-10-02 22:42:15 +0000

    42.0 new design whatsapp

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,26 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  redirects: async () => [
-    {
-      source: "/",
-      destination: "/auth",
-      permanent: false,
-    },
-  ],
-  /* config options here */
+  // Redirección inicial
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/auth",
+        permanent: false,
+      },
+    ];
+  },
+
+  // Configuración de webpack para usar SVGR
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/, 
+      use: ["@svgr/webpack"],
+    });
+    return config;
+  },
 };
 
 export default nextConfig;
